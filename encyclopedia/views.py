df2ef771618c527afcd36224f4e61ea60732ec03
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-11 19:21:01 +0530

    Initial commit (fixed structure)

---
+++ encyclopedia/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import markdown
+from . import util
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def wiki(request, name):
+    entrie = util.get_entry(name)
+    if not entrie:
+        return render(request, "encyclopedia/errorpage.html")
+    html_content = markdown.markdown(entrie)
+    return render(request, "encyclopedia/content.html", {
+        "content": html_content
+    })
