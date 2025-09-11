ae01ed3140b9fc15cdfb189eb765be023bbf208e
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-09-11 23:41:12 +0530

    working on newpage

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django import forms
 import markdown
 from . import util
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -11,11 +14,35 @@ def index(request):
 def wiki(request, name):
     entrie = util.get_entry(name)
     if not entrie:
-        return render(request, "encyclopedia/errorpage.html")
+        return render(request, "encyclopedia/errorpage.html",{
+            "message": "This page cannot be found"
+        })
     html_content = markdown.markdown(entrie)
     return render(request, "encyclopedia/content.html", {
         "content": html_content
     })
+class NewPageform(forms.Form):
+    title = forms.CharField(label="Title")
+    description = forms.CharField(label="Description")
 
+    
 def newpage(request):
-    return render(request, "encyclopedia/newpage.html")
+    if request.method == "POST":
+        form = NewPageform(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+        
+        if util.get_entry(title):
+            return render(request, "encyclopedia/errorpage.html", {
+                "message": "This page already exists"
+            })
+        else:
+            util.save_entry(title, description)
+
+            return HttpResponseRedirect(reverse("wiki", args=[title]))
+
+
+    return render(request, "encyclopedia/newpage.html", {
+        "form": NewPageform()
+    })
